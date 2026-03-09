36c8109afabeb1d029a51a3d7a267633f0b2f636
hello <nihao_123123@qq.com>
2026-03-09 12:07:38 +0800

    a new product list management class . considering how to create a recommon system

--- product.py
+++ product.py
@@ -23,3 +23,22 @@ class PRODUCT:
         self.type = type
     def set_price(self,price):
         self.price = price
+
+class product_list_manager():
+    def __init__(self):
+        self._product_list = []
+    def add_product(self,product):
+        self._product_list.append(product)
+    def get_product_list(self):
+        return self._product_list
+    def get_product_by_name(self,name):
+        for product in self._product_list:
+            if product.get_name() == name:
+                return product
+        return None
+    def delete_product_by_name(self,name):
+        for product in self._product_list:
+            if product.get_name() == name:
+                self._product_list.remove(product)
+                return True
+        return False
--- recommand_system.py
+++ recommand_system.py
@@ -1,12 +1,8 @@
 from user import user
-
+from product import PRODUCT, product_list_manager
 
 class recommondation_system():
     def __init__(self,user):
         self._user = user
     def recommend(self):
-        perfer_item = self._user.get_perfer_item()
-        recommend_list = []
-        for item in perfer_item:
-            recommend_list.append(item)
-        return recommend_list
+        
