c42a9d23af2125158a0cd0d0d3fd69b2fb02b861
hello <nihao_123123@qq.com>
2026-03-09 12:14:50 +0800

    not done yet still considering

--- recommand_system.py
+++ recommand_system.py
@@ -1,8 +1,16 @@
 from user import user
 from product import PRODUCT, product_list_manager
+from Pearson_Correlation_Coefficient import PCC
 
 class recommondation_system():
     def __init__(self,user):
         self._user = user
-    def recommend(self):
-        
+    def recommend(self,user1,user2):
+        pcc = PCC()
+        user1_perfer_item = user1.get_perfer_item()
+        user2_perfer_item = user2.get_perfer_item()
+        result = pcc.calculate(user1_perfer_item,user2_perfer_item)
+        if result > 0:
+            return True
+        else:
+            return False
